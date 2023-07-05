68c5208b8ec35cbf1754ed3f8452c23e42d48902
Daniel Rodriguez <danielrodriguezsuarezbustillo@gmail.com>
2023-07-05 11:25:45 +0200

    Definición de las propiedades de Ficha

--- PawnMaster/Model/Ficha.cs
+++ PawnMaster/Model/Ficha.cs
@@ -3,10 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using PawnMaster;
 
 namespace PawnMaster.Model
 {
     public class Ficha
     {
+        public Color color { get; }
+
+        public  char simbolo { get; set; }
+
+        public int numeroMovimientos { get; set; } = 0;
+
+        //public int[] posicionInicial { get; set; }
+        public int[] posicionActual { get; set; }
+
+        public Ficha(Color color)
+        {
+            this.color = color;
+        }
+
+        public void aumentarNumeroMovimientos()
+        {
+            numeroMovimientos++;
+        }
     }
 }
