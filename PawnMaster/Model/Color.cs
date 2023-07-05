5d54747c8f172e33401a2ed70e1217713335c5f4
Daniel Rodriguez <danielrodriguezsuarezbustillo@gmail.com>
2023-07-05 11:24:27 +0200

    Definición del Modelo de Color

---
+++ PawnMaster/Model/Color.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PawnMaster.Model
+{
+    public class Color
+    {
+        private char LetraRepresentante { get; set; }
+        private Color(char letra)
+        {
+            LetraRepresentante = letra;
+        }
+
+        public override string ToString()
+        {
+            return LetraRepresentante.ToString();
+        }
+
+        public static readonly Color Blanco = new Color('w');
+        public static readonly Color Negro = new Color('b');
+    }
+}
