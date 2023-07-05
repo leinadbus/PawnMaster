7fc8686b9ea60c2ae26f36e45bdec2b60e6fab9b
Daniel Rodriguez <danielrodriguezsuarezbustillo@gmail.com>
2023-07-05 11:24:07 +0200

    Definición del Modelo de Casilla

---
+++ PawnMaster/Model/Casilla.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PawnMaster.Model
+{
+    public class Casilla
+    {
+        // Qué es una casilla? (de ajedrez)
+
+        // Es un espacio donde puede haber una ficha o no
+
+        // Cómo es una casilla?
+        // Qué propiedades tiene una casilla? Cómo la puedo definir?
+
+        /*
+        Tienen coordenadas, que son como un identificador único
+        Las casillas tienen colores (los colores dan igual para el juego [más o menos])
+        Son cuadradas
+        Limitan con otras casillas normalmente
+         */
+
+        public Coordenadas Coordenadas { get; init; }
+        public Color Color { get; set; }
+
+        // La forma no es útil en este progama
+        //public int Forma { get; set; }
+
+        public Ficha? FichaActual { get; private set; } = null;
+
+
+        public Casilla(Coordenadas coordenadas, Color color)
+        {
+            this.Coordenadas = coordenadas;
+            this.Color = color;
+        }
+
+        public void EliminarFicha()
+        {
+            this.FichaActual = null;
+        }
+
+        public void SetFichaActual(Ficha ficha)
+        {
+            this.FichaActual = ficha;
+        }
+    }
+}
