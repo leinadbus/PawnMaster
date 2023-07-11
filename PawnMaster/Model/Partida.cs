14015b836737b1c6d3cec21be83c0303df586107
Daniel Rodriguez <danielrodriguezsuarezbustillo@gmail.com>
2023-07-11 11:33:04 +0200

    Cambios en los tipos de la función CrearPartidaDeAjedrez

--- PawnMaster/Model/Partida.cs
+++ PawnMaster/Model/Partida.cs
@@ -79,43 +79,43 @@ namespace PawnMaster.Model
             var AlfinBlanco1 = new Alfil(Color.Blanco);
 
             //Posicionamiento de las fichas Negras
-            this.Tablero.AñadirFichaAlTablero(0, 0, TorreNegra0);
-            this.Tablero.AñadirFichaAlTablero(1, 0, CaballoNegro0);
-            this.Tablero.AñadirFichaAlTablero(2, 0, AlfinNegro0);
-            this.Tablero.AñadirFichaAlTablero(3, 0, ReinaNegra);
-            this.Tablero.AñadirFichaAlTablero(4, 0, ReyNegro);
-            this.Tablero.AñadirFichaAlTablero(5, 0, AlfinNegro1);
-            this.Tablero.AñadirFichaAlTablero(6, 0, CaballoNegro1);
-            this.Tablero.AñadirFichaAlTablero(7, 0, TorreNegra1);
+            this.Tablero.AñadirFichaAlTablero(new Coordenada('A', 8), TorreNegra0);
+            this.Tablero.AñadirFichaAlTablero(new Coordenada('B', 8), CaballoNegro0);
+            this.Tablero.AñadirFichaAlTablero(new Coordenada('C', 8), AlfinNegro0);
+            this.Tablero.AñadirFichaAlTablero(new Coordenada('D', 8), ReinaNegra);
+            this.Tablero.AñadirFichaAlTablero(new Coordenada('E', 8), ReyNegro);
+            this.Tablero.AñadirFichaAlTablero(new Coordenada('F', 8), AlfinNegro1);
+            this.Tablero.AñadirFichaAlTablero(new Coordenada('G', 8), CaballoNegro1);
+            this.Tablero.AñadirFichaAlTablero(new Coordenada('H', 8), TorreNegra1);
 
-            this.Tablero.AñadirFichaAlTablero(0, 1, PeonNegro0);
-            this.Tablero.AñadirFichaAlTablero(1, 1, PeonNegro1);
-            this.Tablero.AñadirFichaAlTablero(2, 1, PeonNegro2);
-            this.Tablero.AñadirFichaAlTablero(3, 1, PeonNegro3);
-            this.Tablero.AñadirFichaAlTablero(4, 1, PeonNegro4);
-            this.Tablero.AñadirFichaAlTablero(5, 1, PeonNegro5);
-            this.Tablero.AñadirFichaAlTablero(6, 1, PeonNegro6);
-            this.Tablero.AñadirFichaAlTablero(7, 1, PeonNegro7);
+            this.Tablero.AñadirFichaAlTablero(new Coordenada('A', 7), PeonNegro0);
+            this.Tablero.AñadirFichaAlTablero(new Coordenada('B', 7), PeonNegro1);
+            this.Tablero.AñadirFichaAlTablero(new Coordenada('C', 7), PeonNegro2);
+            this.Tablero.AñadirFichaAlTablero(new Coordenada('D', 7), PeonNegro3);
+            this.Tablero.AñadirFichaAlTablero(new Coordenada('E', 7), PeonNegro4);
+            this.Tablero.AñadirFichaAlTablero(new Coordenada('F', 7), PeonNegro5);
+            this.Tablero.AñadirFichaAlTablero(new Coordenada('G', 7), PeonNegro6);
+            this.Tablero.AñadirFichaAlTablero(new Coordenada('H', 7), PeonNegro7);
 
 
-            //Posicionamiento de las fichas Blancas
-            this.Tablero.AñadirFichaAlTablero(0, 7, TorreBlanco0);
-            this.Tablero.AñadirFichaAlTablero(1, 7, CaballoBlanco0);
-            this.Tablero.AñadirFichaAlTablero(2, 7, AlfinBlanco0);
-            this.Tablero.AñadirFichaAlTablero(3, 7, ReinaBlanco);
-            this.Tablero.AñadirFichaAlTablero(4, 7, ReyBlanco);
-            this.Tablero.AñadirFichaAlTablero(5, 7, AlfinBlanco1);
-            this.Tablero.AñadirFichaAlTablero(6, 7, CaballoBlanco1);
-            this.Tablero.AñadirFichaAlTablero(7, 7, TorreBlanco1);
+            ////Posicionamiento de las fichas Blancas
+            this.Tablero.AñadirFichaAlTablero(new Coordenada('A', 1), TorreBlanco0);
+            this.Tablero.AñadirFichaAlTablero(new Coordenada('B', 1), CaballoBlanco0);
+            this.Tablero.AñadirFichaAlTablero(new Coordenada('C', 1), AlfinBlanco0);
+            this.Tablero.AñadirFichaAlTablero(new Coordenada('D', 1), ReinaBlanco);
+            this.Tablero.AñadirFichaAlTablero(new Coordenada('E', 1), ReyBlanco);
+            this.Tablero.AñadirFichaAlTablero(new Coordenada('F', 1), AlfinBlanco1);
+            this.Tablero.AñadirFichaAlTablero(new Coordenada('G', 1), CaballoBlanco1);
+            this.Tablero.AñadirFichaAlTablero(new Coordenada('H', 1), TorreBlanco1);
 
-            this.Tablero.AñadirFichaAlTablero(0, 6, PeonBlanco0);
-            this.Tablero.AñadirFichaAlTablero(1, 6, PeonBlanco1);
-            this.Tablero.AñadirFichaAlTablero(2, 6, PeonBlanco2);
-            this.Tablero.AñadirFichaAlTablero(3, 6, PeonBlanco3);
-            this.Tablero.AñadirFichaAlTablero(4, 6, PeonBlanco4);
-            this.Tablero.AñadirFichaAlTablero(5, 6, PeonBlanco5);
-            this.Tablero.AñadirFichaAlTablero(6, 6, PeonBlanco6);
-            this.Tablero.AñadirFichaAlTablero(7, 6, PeonBlanco7);
+            this.Tablero.AñadirFichaAlTablero(new Coordenada('A', 2), PeonBlanco0);
+            this.Tablero.AñadirFichaAlTablero(new Coordenada('B', 2), PeonBlanco1);
+            this.Tablero.AñadirFichaAlTablero(new Coordenada('C', 2), PeonBlanco2);
+            this.Tablero.AñadirFichaAlTablero(new Coordenada('D', 2), PeonBlanco3);
+            this.Tablero.AñadirFichaAlTablero(new Coordenada('E', 2), PeonBlanco4);
+            this.Tablero.AñadirFichaAlTablero(new Coordenada('F', 2), PeonBlanco5);
+            this.Tablero.AñadirFichaAlTablero(new Coordenada('G', 2), PeonBlanco6);
+            this.Tablero.AñadirFichaAlTablero(new Coordenada('H', 2), PeonBlanco7);
 
         }
     }
