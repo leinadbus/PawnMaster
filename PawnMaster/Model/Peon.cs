0cb446e7988ca0ff4cde8b94b314d875b9f9b360
Daniel Rodriguez <danielrodriguezsuarezbustillo@gmail.com>
2023-07-18 14:00:47 +0200

    Corrección funciones Clase Peon

--- PawnMaster/Model/Peon.cs
+++ PawnMaster/Model/Peon.cs
@@ -15,7 +15,7 @@ namespace PawnMaster.Model
         }
 
 
-        public override bool ValidarPosicion (Casilla casillaEnLaQueEstoy, Casilla casillaALaQuePretendoMoverme)
+        public override bool ValidarMovimiento(Casilla casillaEnLaQueEstoy, Casilla casillaALaQuePretendoMoverme)
         {
             bool sePodriaMover = false;
 
@@ -31,57 +31,69 @@ namespace PawnMaster.Model
             var DiferenciaPosicionesFilas = FilaPosicionfinal - FilaPosicionActual;
             var DiferenciaPosicionesColumnas = ColumnaPosicionFinal - ColumnaPosicionActual;
 
-            if (DiferenciaPosicionesFilas == 1 || DiferenciaPosicionesFilas == 2 || DiferenciaPosicionesFilas == -1 || DiferenciaPosicionesFilas == -2)  //n1 La cantidad de casillas es correcta?
+            //if(NumeroMovimientos == 0 && DiferenciaPosicionesFilas == 1 || DiferenciaPosicionesFilas == 2) { }
+            if (Color == Color.Blanco && NumeroMovimientos == 0 && DiferenciaPosicionesColumnas == 0 && (DiferenciaPosicionesFilas == 1 || DiferenciaPosicionesFilas == 2))
             {
-                if (Color == Color.Blanco)
-                {
-                    if (NumeroMovimientos == 0 && DiferenciaPosicionesFilas == 2 && DiferenciaPosicionesColumnas == 0)
-                    {
-                        sePodriaMover = true;
-                    }
-                    else if (NumeroMovimientos == 0 && DiferenciaPosicionesFilas == 1 && DiferenciaPosicionesColumnas == 0 )
-                    {
-                        sePodriaMover = true;
-                    }
-                }
-                else
-                {
-                    if (NumeroMovimientos == 0 && DiferenciaPosicionesFilas == -2 && DiferenciaPosicionesColumnas == 0)
-                    {
-                        sePodriaMover = true;
-                    }
-                    else if (NumeroMovimientos == 0 && DiferenciaPosicionesFilas == -1 && DiferenciaPosicionesColumnas == 0)
-                    {
-                        sePodriaMover = true;
-                    }
-                }
+                    sePodriaMover = true;
             }
+            else if (Color == Color.Blanco && NumeroMovimientos != 0 && DiferenciaPosicionesColumnas == 0 && DiferenciaPosicionesFilas == 1)
+            {
+                sePodriaMover = true;
+            }
+            else if (Color == Color.Negro && NumeroMovimientos == 0 && DiferenciaPosicionesColumnas == 0 && (DiferenciaPosicionesFilas == -1 || DiferenciaPosicionesFilas == -2))
+            {
+                    sePodriaMover = true;
+            }
+            else if (Color == Color.Negro && NumeroMovimientos != 0 && DiferenciaPosicionesColumnas == 0 && DiferenciaPosicionesFilas == -1)
+            {
+                sePodriaMover = true;
+            }
+
             return sePodriaMover;
         }
 
-
-        public override bool ValidarDireccion(Casilla casillaEnLaQueEstoy, Casilla casillaALaQuePretendoMoverme)
+        public override bool validarCaptura(Casilla casillaEnLaQueEstoy, Casilla casillaALaQuePretendoMoverme)
         {
+            //Mover a clase PEON ------------------------------------------------------------------------------------------------------------------
             bool sePodriaMover = false;
 
             //Posicion Inicial
             var FilaPosicionActual = casillaEnLaQueEstoy.Coordenadas.PosicionVertical;
+            var ColumnaPosicionActual = casillaEnLaQueEstoy.Coordenadas.PosicionHorizontal;
 
             //Posicion Final
             var FilaPosicionfinal = casillaALaQuePretendoMoverme.Coordenadas.PosicionVertical;
+            var ColumnaPosicionFinal = casillaALaQuePretendoMoverme.Coordenadas.PosicionHorizontal;
 
             //Diferencia de filas (Dirección)
             var DiferenciaPosicionesFilas = FilaPosicionfinal - FilaPosicionActual;
+            var DiferenciaPosicionesColumnas = ColumnaPosicionFinal - ColumnaPosicionActual;
 
-            if (Color == Color.Blanco && SaberSiElMovimientoEsPositivo(DiferenciaPosicionesFilas))
-            {
-                    sePodriaMover = true;
-            }
-            else if (Color == Color.Negro && !SaberSiElMovimientoEsPositivo(DiferenciaPosicionesFilas))
-            {
-                    sePodriaMover = true;
-            }
 
+
+            if (DiferenciaPosicionesFilas == 1 || DiferenciaPosicionesFilas == 2 || DiferenciaPosicionesFilas == -1 || DiferenciaPosicionesFilas == -2)  //n1 La cantidad de casillas es correcta?
+            {
+                if (casillaEnLaQueEstoy.FichaActual != null)
+                {
+
+                    if (casillaEnLaQueEstoy.FichaActual.Color == Color.Blanco)
+                    {
+
+                        if (DiferenciaPosicionesFilas == 1 && (DiferenciaPosicionesColumnas == 1 || DiferenciaPosicionesColumnas == -1))
+                        {
+                            sePodriaMover = true;
+                        }
+                    }
+                    else
+                    {
+
+                        if (DiferenciaPosicionesFilas == -1 && (DiferenciaPosicionesColumnas == 1 || DiferenciaPosicionesColumnas == -1))
+                        {
+                            sePodriaMover = true;
+                        }
+                    }
+                }
+            }
             return sePodriaMover;
         }
 
