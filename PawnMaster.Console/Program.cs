3ce50c69811f3f0d7195a0bd7b3d8c7d09933293
Daniel Rodriguez <danielrodriguezsuarezbustillo@gmail.com>
2023-07-18 20:36:21 +0200

    Creación proyecto que ejecuta en consola las dependencias de PawnMaster

---
+++ PawnMaster.Console/Program.cs
@@ -0,0 +1,20 @@
+﻿using PawnMaster.Model;
+
+Jugador jugador = new Jugador { Nombre = "Daniel" };
+Jugador jugadora = new Jugador { Nombre = "Sergio tenia razon" };
+Partida partida = new(jugador, jugadora);
+partida.CrearPartidaDeAjedrez();
+partida.MostrarEstadoPartida();
+
+
+
+string movimientoUsuario;
+do
+{
+    movimientoUsuario = Console.ReadLine();
+
+    partida.EjecutarTurno(movimientoUsuario);
+
+    partida.MostrarEstadoPartida();
+
+} while (movimientoUsuario != "-1");
