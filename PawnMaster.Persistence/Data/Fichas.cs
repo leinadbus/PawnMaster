3192782ca131e04b668210289127dac4cb805136
Daniel Rodriguez <danielrodriguezsuarezbustillo@gmail.com>
2023-09-12 20:16:28 +0200

    Añadida clase Persistencia Fichas

---
+++ PawnMaster.Persistence/Data/Fichas.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PawnMaster.Persistence.Data
+{
+    internal class Fichas
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [ForeignKey("partidaId")]
+        public int partidaId { get; set; }
+        public Partidas Partida { get; set; }
+
+        public enum TipoFicha { P, K, Q, R, N, B }
+        public enum ColorFicha { White, Black }
+        public int NumeroMovimientos { get; set; }
+        public char PosiciónHorizontal { get; set; } 
+        public int PosiciónVertical { get; set; }
+        public bool EnJuego { get; set; }
+
+    }
+}
