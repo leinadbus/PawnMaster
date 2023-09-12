0ed45687ee10a108626d9ef9ed2475e902420512
Daniel Rodriguez <danielrodriguezsuarezbustillo@gmail.com>
2023-09-12 20:07:01 +0200

    Añadida clase Persistencia Partidas

---
+++ PawnMaster.Persistence/Data/Partidas.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PawnMaster.Persistence.Data
+{
+    internal class Partidas
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public enum Turno { blancas, negras }
+
+        public TimeOnly TiempoDeJuego { get; set; }
+
+        [ForeignKey("jugadorBlancoId")]
+        public int jugadorBlancoId { get; set; }
+        public Jugadores JugadorB { get; set; }
+
+        [ForeignKey("jugadorNegroId")]
+        public int jugadorNegroId { get; set; }
+        public Jugadores JugadorN { get; set; }
+    }
+}
