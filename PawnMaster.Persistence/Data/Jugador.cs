c7917739f67bb6bd57adf45b6dd28e389741813f
Daniel Rodriguez <danielrodriguezsuarezbustillo@gmail.com>
2023-09-14 20:41:04 +0200

    Refactorización Jugador en la capa de Persistencia

--- PawnMaster.Persistence/Data/Jugador.cs
+++ PawnMaster.Persistence/Data/Jugador.cs
@@ -7,16 +7,15 @@ using System.Threading.Tasks;
 
 namespace PawnMaster.Persistence.Data
 {
-    public class Jugadores
+    public class Jugador
     {
-        [Key]
         public int Id { get; set; }
         public string Nombre { get; set; }
         public string Correo { get; set; }
         public string Password { get; set; }
         public byte[] RutaImagen { get; set; }
-        public int PartidasJugadas { get; set; }
-        public int PartidasGanadas { get; set; }
+        public ICollection<Partida> PartidasJugadasComoBlancas { get; set; }
+        public ICollection<Partida> PartidasJugadasComoNegras { get; set; }
         public DateTime CreacionCuenta { get; set; } = DateTime.Now;
         public string Role { get; set; }
 
