11b4e74f06bbce1a98b6b48602f6a60933e3da19
Daniel Rodriguez <danielrodriguezsuarezbustillo@gmail.com>
2023-09-21 16:53:45 +0200

    Añadidos Dtos de la capa Persistence (UsuarioLogindto y UsuarioRegistroDto)

---
+++ PawnMaster.Persistence/Dtos/UsuarioLoginDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PawnMaster.Persistence.Dtos
+{
+    public class UsuarioLoginDto
+    {
+        [Required(ErrorMessage = "El correo es obligatorio")]
+        public string CorreoElectronico { get; set; }
+        [Required(ErrorMessage = "La contraseña es obligatorio")]
+        public string Password { get; set; }
+    }
+}
---
+++ PawnMaster.Persistence/Dtos/UsuarioRegistroDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PawnMaster.Persistence.Dtos
+{
+    public class UsuarioRegistroDto
+    {
+        [Required(ErrorMessage = "El correo es obligatorio")]
+        public string CorreoElectronico { get; set; }
+        [Required(ErrorMessage = "El nombre es obligatorio")]
+        public string Nombre { get; set; }
+        [Required(ErrorMessage = "La contraseña es obligatorio")]
+        public string Password { get; set; }
+        public string Role { get; set; }
+    }
+}
