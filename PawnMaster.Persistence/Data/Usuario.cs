b2a67d6f53a83c528bec5b17ee98167a0d371072
Daniel Rodriguez <danielrodriguezsuarezbustillo@gmail.com>
2023-09-21 16:36:52 +0200

    Refactorización Usuario BD anteriormente Jugador

--- PawnMaster.Persistence/Data/Usuario.cs
+++ PawnMaster.Persistence/Data/Usuario.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using NHibernate.Util;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -7,14 +9,14 @@ using System.Threading.Tasks;
 
 namespace PawnMaster.Persistence.Data
 {
-    public class Jugador
+    public class Usuario 
     {
         public int Id { get; set; }
         public string Nombre { get; set; }
         public string Correo { get; set; }
         public string Password { get; set; }
-        public byte[] RutaImagen { get; set; }
-        public ICollection<Partida> PartidasJugadasComoBlancas { get; set; }
+        public byte[]? RutaImagen { get; set; }
+        public ICollection<Partida> PartidasJugadasComoBlancas { get; set; } 
         public ICollection<Partida> PartidasJugadasComoNegras { get; set; }
         public DateTime CreacionCuenta { get; set; } = DateTime.Now;
         public string Role { get; set; }
---
+++ PawnMaster.Persistence/Migrations/20230921143202_InitialMigration.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PawnMaster.Persistence.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Jugadores",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Correo = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    RutaImagen = table.Column<byte[]>(type: "varbinary(max)", nullable: true),
+                    CreacionCuenta = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Role = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Jugadores", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Partidas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    TiempoDeJuego = table.Column<TimeSpan>(type: "time", nullable: false),
+                    PartidaEnJuego = table.Column<bool>(type: "bit", nullable: false),
+                    Ganador = table.Column<int>(type: "int", nullable: false),
+                    JugadorBlancoId = table.Column<int>(type: "int", nullable: false),
+                    JugadorNegroId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Partidas", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Partidas_Jugadores_JugadorBlancoId",
+                        column: x => x.JugadorBlancoId,
+                        principalTable: "Jugadores",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Partidas_Jugadores_JugadorNegroId",
+                        column: x => x.JugadorNegroId,
+                        principalTable: "Jugadores",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Fichas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    partidaId = table.Column<int>(type: "int", nullable: false),
+                    NumeroMovimientos = table.Column<int>(type: "int", nullable: false),
+                    PosiciónHorizontal = table.Column<string>(type: "nvarchar(1)", nullable: false),
+                    PosiciónVertical = table.Column<int>(type: "int", nullable: false),
+                    EnJuego = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Fichas", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Fichas_Partidas_partidaId",
+                        column: x => x.partidaId,
+                        principalTable: "Partidas",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Fichas_partidaId",
+                table: "Fichas",
+                column: "partidaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Partidas_JugadorBlancoId",
+                table: "Partidas",
+                column: "JugadorBlancoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Partidas_JugadorNegroId",
+                table: "Partidas",
+                column: "JugadorNegroId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Fichas");
+
+            migrationBuilder.DropTable(
+                name: "Partidas");
+
+            migrationBuilder.DropTable(
+                name: "Jugadores");
+        }
+    }
+}
