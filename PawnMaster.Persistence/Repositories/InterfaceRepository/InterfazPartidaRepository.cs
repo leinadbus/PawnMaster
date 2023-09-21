e8e46299071883f8346349497feaa9fdf7f7bd9d
Daniel Rodriguez <danielrodriguezsuarezbustillo@gmail.com>
2023-09-21 17:05:22 +0200

    Creada interfaz InterfazPartidaRepository

---
+++ PawnMaster.Persistence/Repositories/InterfaceRepository/InterfazPartidaRepository.cs
@@ -0,0 +1,15 @@
+﻿using PawnMaster.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PawnMaster.Persistence.Repositories.InterfaceRepository
+{
+    internal interface InterfazPartidaRepository
+    {
+        bool GuardarEstadoPartida(Partida partida);
+        Partida RecuperarEstadoPartida (int id);    
+    }
+}
