2edc0079acc876034af1c921e81122851e0007c0
Daniel Rodriguez <danielrodriguezsuarezbustillo@gmail.com>
2023-09-21 17:05:03 +0200

    Creado controlador PartidasController

---
+++ PawnMaster.API/Controllers/PartidasController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PawnMaster.API.Dtos;
+
+namespace PawnMaster.API.Controllers
+{
+    [Route("api/partidas")]
+    [ApiController]
+    public class PartidasController : ControllerBase
+    {
+
+        protected RespuestaApi _respuestaApi;
+    }
+}
