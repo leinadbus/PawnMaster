3e97e66992b7151b331a9d4dd6b8743faa5d018d
Daniel Rodriguez <danielrodriguezsuarezbustillo@gmail.com>
2023-09-21 16:52:42 +0200

    Añadida InterfazUsuarioRepository

---
+++ PawnMaster.Persistence/Repositories/InterfaceRepository/InterfazUsuarioRepository.cs
@@ -0,0 +1,20 @@
+﻿using PawnMaster.Persistence.Data;
+using PawnMaster.Persistence.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PawnMaster.Persistence.Repositories.InterfaceRepository
+{
+    public interface InterfazUsuarioRepository
+    {
+        ICollection<Usuario> GetJugadores();
+        Usuario GetJugador(int jugadorId);
+        bool EsCorreoUnico(string correo);
+        bool Registro(UsuarioRegistroDto usuarioRegistroDto);
+
+
+    }
+}
