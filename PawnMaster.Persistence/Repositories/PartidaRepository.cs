e6caaf0ca3e49dafcfec3e01ff979cad2072aac2
Daniel Rodriguez <danielrodriguezsuarezbustillo@gmail.com>
2023-09-21 19:45:09 +0200

    Añadido PartidaRepository

---
+++ PawnMaster.Persistence/Repositories/PartidaRepository.cs
@@ -0,0 +1,30 @@
+﻿using PawnMaster.Model;
+using PawnMaster.Persistence.Dtos;
+using PawnMaster.Persistence.Repositories.InterfaceRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PawnMaster.Persistence.Repositories
+{
+    public class PartidaRepository : InterfazPartidaRepository
+    {
+        public Partida Partida { get; set; }
+        public bool MantenerEstadoPartida(Partida partida)
+        {
+            Partida = partida;
+            return true;
+        }
+        public bool GuardarEstadoPartida(Partida partida)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Partida RecuperarEstadoPartida(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
