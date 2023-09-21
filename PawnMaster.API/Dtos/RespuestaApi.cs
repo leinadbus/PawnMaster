9563c8f140c2d388d1264500290e98b364b2b2d5
Daniel Rodriguez <danielrodriguezsuarezbustillo@gmail.com>
2023-09-21 16:54:15 +0200

    Añadidos Dtos de la capa API (UsusarioRegistroAPIDto y RespuestaAPIDto)

---
+++ PawnMaster.API/Dtos/RespuestaApi.cs
@@ -0,0 +1,17 @@
+﻿using System.Net;
+
+namespace PawnMaster.API.Dtos
+{
+    public class RespuestaApi
+    {
+        public RespuestaApi()
+        {
+            ErrorMessages = new List<string>();
+        }
+
+        public HttpStatusCode StatusCode { get; set; }
+        public bool IsSuccess { get; set; } = true;
+        public List<string> ErrorMessages { get; set; }
+        public object Result { get; set; }
+    }
+}
---
+++ PawnMaster.API/Dtos/UsuarioRegistroAPIDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PawnMaster.API.Dtos
+{
+    public class UsuarioRegistroAPIDto
+    {
+        [Required(ErrorMessage = "El correo es obligatorio")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "El nombre es obligatorio")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "La contraseña es obligatorio")]
+        public string Contraseña { get; set; }
+        public string Role { get; set; }
+    }
+}
