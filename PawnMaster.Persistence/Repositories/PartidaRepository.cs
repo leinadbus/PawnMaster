c21f7bd1c3ad09fbdd1304f687cc9dde9e40bd2a
Daniel Rodriguez <danielrodriguezsuarezbustillo@gmail.com>
2023-09-25 19:28:20 +0200

    Añadida función CrearPartidaen Base de Datos

--- PawnMaster.Persistence/Repositories/PartidaRepository.cs
+++ PawnMaster.Persistence/Repositories/PartidaRepository.cs
@@ -1,4 +1,5 @@
 ﻿using PawnMaster.Model;
+using PawnMaster.Persistence.Data;
 using PawnMaster.Persistence.Dtos;
 using PawnMaster.Persistence.Repositories.InterfaceRepository;
 using System;
@@ -11,18 +12,36 @@ namespace PawnMaster.Persistence.Repositories
 {
     public class PartidaRepository : InterfazPartidaRepository
     {
-        public Partida Partida { get; set; }
-        public bool MantenerEstadoPartida(Partida partida)
+        private readonly ApplicationDbContext _bd;
+        public PartidaRepository(ApplicationDbContext bd)
         {
-            Partida = partida;
-            return true;
+            this._bd = bd;
         }
-        public bool GuardarEstadoPartida(Partida partida)
+
+        public bool CrearPartida(PartidaDto Partida,int IdBlanco, int IdNegro)
         {
+            //Pasarla a objeto de _Bd
+            var PartidaABaseDatos = new Data.Partida() {
+                JugadorBlancoId = IdBlanco,
+                JugadorNegroId = IdNegro,
+                PartidaEnJuego = true,
+                TiempoDeJuego = DateTime.Now - Partida.Date,
+                TurnoPartida = Data.Partida.Turno.white
+            };
+         
+            _bd.Partidas.Add(PartidaABaseDatos);
+            _bd.SaveChanges();
+            return true;
+            //throw new NotImplementedException();
+        }
+
+        public bool GuardarEstadoPartida(PartidaDto partida)
+        {
+            //partida.
             throw new NotImplementedException();
         }
 
-        public Partida RecuperarEstadoPartida(int id)
+        public PartidaDto RecuperarEstadoPartida(int id)
         {
             throw new NotImplementedException();
         }
