c794f9f3fbd830346655e6bf58bb6e039b26e9ee
Daniel Rodriguez <danielrodriguezsuarezbustillo@gmail.com>
2023-09-25 19:28:40 +0200

    Añadida clase PartidaDto

---
+++ PawnMaster.Persistence/Dtos/PartidaDto.cs
@@ -0,0 +1,25 @@
+﻿using PawnMaster.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PawnMaster.Persistence.Dtos
+{
+    public class PartidaDto
+    {
+        public Guid Identificador { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public Tablero Tablero { get; set; }
+
+        public Jugador JugadorBlanco { get; set; }
+        public Jugador JugadorNegro { get; set; }
+
+        public List<string> ListaDeMovimientos { get; set; }
+
+        public Jugador JugadorActual { get; set; }
+    }
+}
