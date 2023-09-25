49cc8d0212fbbbe89a30b2b8d8adf6af398fdfff
Daniel Rodriguez <danielrodriguezsuarezbustillo@gmail.com>
2023-09-25 19:29:53 +0200

    Refactorización tipo de dato TurnoJuego en Partida

--- PawnMaster.Persistence/Data/Partida.cs
+++ PawnMaster.Persistence/Data/Partida.cs
@@ -15,10 +15,11 @@ namespace PawnMaster.Persistence.Data
     {
 
         public int Id { get; set; }
-        public enum Turno { white, black }
+        public enum Turno {white,black}
+        public Turno TurnoPartida { get; set; }
         public TimeSpan TiempoDeJuego { get; set; }
         public bool PartidaEnJuego { get; set; }
-        public int Ganador { get; set; }
+        public int? Ganador { get; set; }
        
         public int JugadorBlancoId { get; set; }
         public Usuario JugadorBlanco { get; set; }
