387006518618ecfd05dd4c0de0bec8eadde5fdd9
Daniel Rodriguez <danielrodriguezsuarezbustillo@gmail.com>
2023-09-25 19:29:01 +0200

    añadida clase PArtidaDtoAPI

---
+++ PawnMaster.API/Dtos/PartidaDtoAPI.cs
@@ -0,0 +1,20 @@
+﻿using PawnMaster.Model;
+
+namespace PawnMaster.API.Dtos
+{
+    public class PartidaDtoAPI
+    {
+        public Guid Identificador { get; set; }
+
+        public DateTime Date { get; set; }
+
+        private Tablero Tablero { get; set; }
+
+        public Jugador JugadorBlanco { get; set; }
+        public Jugador JugadorNegro { get; set; }
+
+        public List<string> ListaDeMovimientos { get; set; }
+
+        public Jugador JugadorActual { get; set; }
+    }
+}
