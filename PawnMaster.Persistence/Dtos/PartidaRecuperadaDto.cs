b623378caab91526b64563de21125b5bcc3fa70d
Daniel Rodriguez <danielrodriguezsuarezbustillo@gmail.com>
2023-09-26 19:44:37 +0200

    Añadida clase PartidaRecuperadaDto a Dtos de Persistence

---
+++ PawnMaster.Persistence/Dtos/PartidaRecuperadaDto.cs
@@ -0,0 +1,26 @@
+﻿using PawnMaster.Model;
+using PawnMaster.Persistence.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PawnMaster.Persistence.Dtos
+{
+    public class PartidaRecuperadaDto
+    {
+        public int Identificador { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public Tablero Tablero { get; set; }
+
+        public Usuario JugadorBlanco { get; set; }
+        public Usuario JugadorNegro { get; set; }
+
+        public List<string> ListaDeMovimientos { get; set; }
+        public enum Turno { white, black }
+        public Turno TurnoPartida { get; set; }
+    }
+}
