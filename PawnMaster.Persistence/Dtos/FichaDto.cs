25aa9e22670f6b001a7699a88844f18f003521df
Daniel Rodriguez <danielrodriguezsuarezbustillo@gmail.com>
2023-10-10 20:37:43 +0200

    Añadida clase FichaDto a capa Persistence

---
+++ PawnMaster.Persistence/Dtos/FichaDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PawnMaster.Persistence.Dtos
+{
+    public class FichaDto
+    {
+        public int PosicionVertical { get; set; }
+        public char PosicionHorizontal { get; set; }
+        public char Simbolo { get; set; }
+        public char LetraRepresentante { get; set; }
+    }
+}
