f5e0c5a6f716b6c868906d6f387420253255e97a
Daniel Rodriguez <danielrodriguezsuarezbustillo@gmail.com>
2023-10-11 16:55:15 +0200

    Añadido Dto PartidaDataDto

---
+++ PawnMaster.Persistence/Dtos/PartidaDataDto.cs
@@ -0,0 +1,17 @@
+﻿using PawnMaster.Model;
+using PawnMaster.Persistence.Dtos;
+
+namespace PawnMaster.Persistence.Dtos
+{
+    public class PartidaDataDto
+    {
+        
+        public DateTime Date { get; set; }
+        public int JugadorBlancoId { get; set; }
+        public int JugadorNegroId { get; set; }
+        public List<FichaDto> ListaDeFichas { get; set; }
+        public char JugadorActual { get; set; }
+        public bool EnJuego { get; set; }
+        public int Id { get; set; }
+    }
+}
