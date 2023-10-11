8f58315f0d6aff46c56cd7224ff5ffe02fb80aa8
Daniel Rodriguez <danielrodriguezsuarezbustillo@gmail.com>
2023-10-11 16:21:49 +0200

    Migración tipo de dato TurnoPartida en Objeto de persistencia Partida

---
+++ PawnMaster.Persistence/Migrations/20231011141355_Refactortype.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PawnMaster.Persistence.Migrations
+{
+    public partial class Refactortype : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "TurnoPartida",
+                table: "Partidas",
+                type: "nvarchar(1)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "TurnoPartida",
+                table: "Partidas",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(1)");
+        }
+    }
+}
