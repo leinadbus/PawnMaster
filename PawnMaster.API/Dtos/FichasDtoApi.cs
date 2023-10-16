7e7e8cd9f296cb3e90fe931c27b72309dd7a3ac8
Daniel Rodriguez <danielrodriguezsuarezbustillo@gmail.com>
2023-10-16 16:24:33 +0200

    Añadida clase FichasDtoAPI

---
+++ PawnMaster.API/Dtos/FichasDtoApi.cs
@@ -0,0 +1,11 @@
+﻿namespace PawnMaster.API.Dtos
+{
+    public class FichasDtoApi
+    {
+        public int PosicionVertical { get; set; }
+        public char PosicionHorizontal { get; set; }
+        public char Simbolo { get; set; }
+        public char LetracolorRepresentante { get; set; }
+        public bool EnJuego { get; set; }
+    }
+}
