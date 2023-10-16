1061aaa757b700c47c9bc0042ee13a4cdac3cf07
Daniel Rodriguez <danielrodriguezsuarezbustillo@gmail.com>
2023-10-16 18:25:14 +0200

    Añadida Clase PArtidasRecuperadasDtoApi

---
+++ PawnMaster.API/Dtos/PartidasRecuperadasDtoAPI.cs
@@ -0,0 +1,13 @@
+﻿using PawnMaster.Model;
+
+namespace PawnMaster.API.Dtos
+{
+    public class PartidasRecuperadasDtoAPI
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public int JugadorBlanco { get; set; }
+        public int JugadorNegro { get; set; }
+        public char JugadorActual { get; set; }
+    }
+}
