18741d12afdca0acd79d448205f77610b0d4039e
Daniel Rodriguez <danielrodriguezsuarezbustillo@gmail.com>
2023-10-16 17:10:30 +0200

    Añadida clase JugadoresDtoApi en capa API

---
+++ PawnMaster.API/Dtos/JugadoresDtoApi.cs
@@ -0,0 +1,10 @@
+﻿namespace PawnMaster.API.Dtos
+{
+    public class JugadoresDtoApi
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Correo { get; set; }
+        public DateTime FechaCreacion { get; set; }
+    }
+}
