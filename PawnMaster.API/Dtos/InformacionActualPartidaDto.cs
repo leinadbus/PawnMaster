595a90d3338f57a248d8a520a3a5d674ef1e8b5c
Daniel Rodriguez <danielrodriguezsuarezbustillo@gmail.com>
2023-10-17 12:50:04 +0200

    Añadida clase InformacionActualPartidaDto a la capa API

---
+++ PawnMaster.API/Dtos/InformacionActualPartidaDto.cs
@@ -0,0 +1,11 @@
+﻿namespace PawnMaster.API.Dtos
+{
+    public class InformacionActualPartidaDto
+    {
+        public char Turno {  get; set; }
+        public DateTime Tiempo { get; set; }
+        public List<FichasDtoApi> ListaFichasEnJuego { get; set; }
+        public List<FichasDtoApi> ListaFichasFueraDeJuego { get; set; }
+
+    }
+}
